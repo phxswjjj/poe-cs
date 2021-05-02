1ba5f61898f598e3a153da47d7b60d959fb7fc8b
phxswjjj <phxswjjj@gmail.com>
2021-05-02 09:24:34 +0800

    capture window

---
+++ poe.lib/Win/ScreenCapture.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace poe.lib.Win
+{
+    public class ScreenCapture
+    {
+        public Image CaptureScreen()
+        {
+            return CaptureWindow(User32.GetDesktopWindow());
+        }
+        public Image CaptureWindow(IntPtr handle)
+        {
+            IntPtr hscrdc = User32.GetWindowDC(handle);
+
+            User32.RECT rect;
+            User32.GetWindowRect(handle, out rect);
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+
+            IntPtr hbitmap = Gdi32.CreateCompatibleBitmap(hscrdc, width, height);
+            IntPtr hmemdc = Gdi32.CreateCompatibleDC(hscrdc);
+            Gdi32.SelectObject(hmemdc, hbitmap);
+            User32.PrintWindow(handle, hmemdc, 0);
+
+            var bmp = Bitmap.FromHbitmap(hbitmap);
+
+            //刪除用過的對象  
+            Gdi32.DeleteDC(hscrdc);
+            Gdi32.DeleteDC(hmemdc);
+
+            return bmp;
+        }
+        public void CaptureWindowToFile(IntPtr handle, string filename, ImageFormat format)
+        {
+            Image img = CaptureWindow(handle);
+            img.Save(filename, format);
+        }
+        public void CaptureScreenToFile(string filename, ImageFormat format)
+        {
+            Image img = CaptureScreen();
+            img.Save(filename, format);
+        }
+
+        private class Gdi32
+        {
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
+            [DllImport("gdi32.dll")]
+            public static extern int DeleteDC(IntPtr hdc);
+        }
+        private class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct RECT
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetDesktopWindow();
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowDC(IntPtr hWnd);
+            [DllImport("user32.dll")]
+            public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, out RECT rect);
+
+            [DllImport("user32.dll")]
+            public static extern bool PrintWindow(IntPtr hwnd, IntPtr hdcBlt, UInt32 nFlags);
+        }
+    }
+}
---
+++ stream-viewer/Form1.cs
@@ -0,0 +1,96 @@
+﻿using poe.lib.Win;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stream_viewer
+{
+    public partial class Form1 : Form
+    {
+        Thread JobRefreshStream = null;
+        bool Stopping = false;
+        bool IsPause = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var missAndRetryTime = 10 * 1000;
+            var findWindowTitle = "LINE";
+            var process = Process.GetProcesses().FirstOrDefault(p => p.MainWindowTitle == findWindowTitle);
+            var capture = new ScreenCapture();
+
+            JobRefreshStream = new Thread(() =>
+            {
+                while (!Stopping)
+                {
+                    if (process == null)
+                    {
+                        //wait retry
+                        Thread.Sleep(missAndRetryTime);
+                        process = Process.GetProcesses().FirstOrDefault(p => p.MainWindowTitle == findWindowTitle);
+                    }
+                    if (!IsPause && process != null)
+                    {
+                        try
+                        {
+                            var img = capture.CaptureWindow(process.MainWindowHandle);
+                            if (pictureBox1.Image != null)
+                                pictureBox1.Image.Dispose();
+                            pictureBox1.Image = img;
+                        }
+                        catch (System.Runtime.InteropServices.ExternalException)
+                        {
+                            process = null;
+                            Thread.Sleep(missAndRetryTime);
+                        }
+                    }
+                    Thread.Sleep(100);
+                }
+            });
+
+            JobRefreshStream.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (JobRefreshStream != null)
+            {
+                Stopping = true;
+                if (!JobRefreshStream.Join(1000))
+                {
+                    try
+                    {
+                        JobRefreshStream.Abort();
+                    }
+                    catch (Exception)
+                    {
+                        //ignore
+                    }
+                }
+
+            }
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            IsPause = true;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            IsPause = false;
+        }
+    }
+}
