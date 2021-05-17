00ab07f80f29a680f9e345aeac80903cc067afc7
phxswjjj <phxswjjj@gmail.com>
2021-05-17 22:47:45 +0800

    使用 Parallel 改善執行效率(效果沒想像中大)

--- stream-viewer/MLModelEngineExtension.cs
+++ stream-viewer/MLModelEngineExtension.cs
@@ -2,6 +2,7 @@
 using poe.lib.ImageExtension;
 using poe.lib.ML;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -14,16 +15,30 @@ namespace stream_viewer
         public static void PredictionImage(this MLModelEngine<ModelInput, ModelOutput> mlEngine,
             Image img)
         {
+            BlockingCollection<dynamic> predictions = new BlockingCollection<dynamic>();
+            var imgRawFormat = img.RawFormat;
+
             var imgRepo = new ScreenRepository(img);
-            foreach (var part in imgRepo.Parts)
+            System.Threading.Tasks.Parallel.ForEach(imgRepo.Parts, part =>
             {
                 ModelInput inputData = new ModelInput();
                 using (var ms = new MemoryStream())
                 {
-                    part.Source.Save(ms, img.RawFormat);
+                    part.Source.Save(ms, imgRawFormat);
                     inputData.Image = ms.ToArray();
                 }
                 ModelOutput prediction = mlEngine.Predict(inputData);
+                predictions.Add(new
+                {
+                    PartImg = part,
+                    Prediction = prediction,
+                });
+            });
+
+            foreach (var d in predictions)
+            {
+                PartScreen part = d.PartImg;
+                ModelOutput prediction = d.Prediction;
                 img.TagImage(part.Location, prediction);
             }
         }
